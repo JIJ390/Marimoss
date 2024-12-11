57f9567d91d26df921becfab34d44649750e9a93
JIJ390 <wwdi1235@gmail.com>
2024-12-11 19:30:17 +0900

    ㅁㅁ

--- src/main/resources/static/js/main/main.js
+++ src/main/resources/static/js/main/main.js
@@ -17,6 +17,8 @@ document.addEventListener('DOMContentLoaded', () => {
       //페이지를 불러오는 함수를 호출합니다.
       console.log(lastCp);
 
+      updateBoardList(lastCp);
+
       
 
     });
@@ -36,34 +38,8 @@ const updateBoardList = (lastCp) => {
     if (resp.ok) return resp.text();
     throw new Error("목록 조회 실패")
   })
-  .then(result => {
-    
-  })
+  .catch(err => console.error)
+
 
 }
 
-
-        // fetch("/admin/adminBrandFilter", {
-        //     method: "POST",
-        //     headers: { "Content-Type": "application/json" },
-        //     body: brandFilter
-        // })
-        //     .then((res) => {
-        //         if (res.ok) return res.text();
-        //         throw new Error("비동기 실패");
-        //     })
-        //     .then(result => {
-        //         console.log(result); // 검색결과 담김.
-        //         console.log("asdsaddads",result); // 검색결과 담김.
-
-        //         const deviceListColorBody = document.querySelector(".deviceListColorBody");
-
-
-        //         console.log(result);
-        //         deviceListColorBody.innerHTML = "";
-        //         deviceListColorBody.innerHTML = result;
-                
-                
-
-        //     })
-        //     .catch(err => { console.log(err); })
