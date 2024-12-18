610d1365ceb67e520af9a1724d9480d1be8042f2
JIJ390 <wwdi1235@gmail.com>
2024-12-18 18:28:15 +0900

    1

--- src/main/java/jij/marimoss/board/controller/BoardController.java
+++ src/main/java/jij/marimoss/board/controller/BoardController.java
@@ -6,14 +6,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 import jij.marimoss.board.service.BoardService;
+import jij.marimoss.member.dto.Member;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("board")
 @RequiredArgsConstructor
+@SessionAttributes({"loginMember"})
 public class BoardController {
 	
 	private final BoardService service;
@@ -31,19 +35,17 @@ public class BoardController {
 	public String BoardInsert (
 			@RequestParam("paint") MultipartFile paint,
 			@RequestParam("boardTitle") String boardTitle,
-			@RequestParam("boardContent") String boardContent
+			@RequestParam("boardContent") String boardContent,
+			@SessionAttribute("loginMember") Member loginMember
 			) {
 		// 로그인 회원정보 추가해야함!!
 		
-		int memberNo = 1;
+		int memberNo = loginMember.getMemberNo();
 		
 		int result = service.boardInsert(paint, boardTitle, boardContent, memberNo);
 		
-		
 		return "1";
 	}
-	
-	
 	
 	
 }
